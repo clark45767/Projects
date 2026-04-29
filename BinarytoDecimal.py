14ffe023f8c8c210cc74cee21a20db607d9d2189
clark45767 <clarkigarma@gmail.com>
2026-04-29 19:02:56 +0400

    Add files via upload

---
+++ BinarytoDecimal.py
@@ -0,0 +1,11 @@
+def binary_to_decimal():
+    binary = input("Enter your Binary: ")
+    
+    decimal = int(binary, 2)
+    
+    print("Decimal :", decimal)
+
+
+
+binary_to_decimal()
+binary_to_decimal()
